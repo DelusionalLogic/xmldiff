d96c0a6e438163b29cec1afeb66c467984587b1a
Jesper Jensen <jesper@jnsn.dev>
2024-09-09 10:25:26 +0200

    First attempt at backtracing

--- constrained.py
+++ constrained.py
@@ -1,38 +1,49 @@
+from collections import (
+    deque,
+)
 from enum import (
     Enum,
     auto,
 )
 
+import edist.alignment
+import edist.sed
+import edist.tree_edits
 import numpy as np
-from edist import (
-    sed,
-)
 
 a = [0, 1, 2, 3]
 a_adj = [[1, 3], [2], [], []]
 
-b = [0, 5, 3, 2, 4]
-b_adj = [[1, 2], [], [3, 4], [], []]
+b = [0, 1, 3, 2, 4, 5]
+b_adj = [[1, 2], [], [3, 4], [], [5], []]
 
 cost_n = np.empty((len(a)+1, len(b)+1), dtype=int)
 cost_f = np.empty((len(a)+1, len(b)+1), dtype=int)
 trace = [[None for _ in range(len(b)+1)] for _ in range(len(a)+1)]
 
 def cost(ai, bi):
-    if ai is None or bi is None:
+    if ai is None:
         return 1
-    return 0 if a[ai] == b[bi] else 1
+    if bi is None:
+        return 1
 
-def initialize_costs():
-    cost_n[0][0] = cost_f[0][0] = 0
-    for i in reversed(range(len(a))):
-        cost_f[i+1][0] = sum(cost_n[c+1][0] for c in a_adj[i])
-        cost_n[i+1][0] = cost_f[i+1][0] + cost(i, None)
-    for j in reversed(range(len(b))):
-        cost_f[0][j+1] = sum(cost_n[0][c+1] for c in b_adj[j])
-        cost_n[0][j+1] = cost_f[0][j+1] + cost(None, j)
+    if a[ai] == b[bi]:
+        return 0
 
-initialize_costs()
+    return 1
+
+cost_n[0][0] = 0
+cost_f[0][0] = 0
+for i in reversed(range(len(a))):
+    cost_f[i+1][0] = 0
+    for c in a_adj[i]:
+        cost_f[i+1][0] += cost_n[c+1][0]
+    cost_n[i+1][0] = cost_f[i+1][0] + cost(i, None)
+for j in reversed(range(len(b))):
+    cost_f[0][j+1] = 0
+    for c in b_adj[j]:
+        cost_f[0][j+1] += cost_n[0][c+1]
+    cost_n[0][j+1] = cost_f[0][j+1] + cost(None, j)
 
 class Cmd(Enum):
     MATCH = 1
@@ -43,9 +54,9 @@ class Cmd(Enum):
 
 def seq_dist(ai, bi):
     if ai is None:
-        return cost_n[0][bi]
+        return cost_n[0][bi+1]
     if bi is None:
-        return cost_n[ai][0]
+        return cost_n[ai+1][0]
 
     return cost_n[ai+1][bi+1]
 
@@ -54,18 +65,20 @@ cost_e = np.empty((len(a)+1, len(b)+1), dtype=int)
 for i in reversed(range(len(a))):
     for j in reversed(range(len(b))):
         choices = [
-            sed.sed(a_adj[i], b_adj[j], delta=seq_dist)
+            edist.sed.sed(a_adj[i], b_adj[j], delta=seq_dist)
         ]
-        alignment = sed.sed_backtrace(a_adj[i], b_adj[j], delta=seq_dist)
+        alignment = edist.sed.sed_backtrace(a_adj[i], b_adj[j], delta=seq_dist)
         f_traces = [
             (Cmd.MATCH, alignment)
         ]
 
+        fmin_s = None
         if len(a_adj[i]) > 0:
             fmin_s = np.argmin((cost_f[a_adj[i][s]+1][j+1] - cost_f[a_adj[i][s]+1][0] for s in range(len(a_adj[i]))))
             choices.append(cost_f[i+1][0] + cost_f[a_adj[i][fmin_s]+1][j+1] - cost_f[a_adj[i][fmin_s]+1][0])
             f_traces.append((Cmd.REMOVE, fmin_s))
 
+        fmin_t = None
         if len(b_adj[j]) > 0:
             fmin_t = np.argmin((cost_f[i+1][b_adj[j][t]+1] - cost_f[0][b_adj[j][t]+1] for t in range(len(b_adj[j]))))
             choices.append(cost_f[0][j+1] + cost_f[i+1][b_adj[j][fmin_t]+1] - cost_f[0][b_adj[j][fmin_t]+1])
@@ -73,7 +86,7 @@ for i in reversed(range(len(a))):
 
         f_min = np.argmin(choices)
         cost_f[i+1][j+1] = choices[f_min]
-        trace[i+1][j+1] = (f_traces[f_min], n_traces[n_min])
+        forest_trace = f_traces[f_min]
 
 
         choices = [
@@ -83,11 +96,13 @@ for i in reversed(range(len(a))):
             (Cmd.MATCH, None)
         ]
 
+        n_min_s = None
         if len(a_adj[i]) > 0:
             n_min_s = np.argmin((cost_n[a_adj[i][s]+1][j+1] - cost_n[a_adj[i][s]+1][0] for s in range(len(a_adj[i]))))
             choices.append(cost_n[i+1][0] + cost_n[a_adj[i][n_min_s]+1][j+1] - cost_n[a_adj[i][n_min_s]+1][0])
             n_traces.append((Cmd.REMOVE, fmin_s))
 
+        n_min_t = None
         if len(b_adj[j]) > 0:
             n_min_t = np.argmin((cost_n[i+1][b_adj[j][t]+1] - cost_n[0][b_adj[j][t]+1] for t in range(len(b_adj[j]))))
             choices.append(cost_n[0][j+1] + cost_n[i+1][b_adj[j][n_min_t]+1] - cost_n[0][b_adj[j][n_min_t]+1])
@@ -95,29 +110,60 @@ for i in reversed(range(len(a))):
 
         n_min = np.argmin(choices)
         cost_n[i+1][j+1] = choices[n_min]
+        trace[i+1][j+1] = (forest_trace, n_traces[n_min])
 
+print(cost_f)
+print(cost_n)
 
-
-to_compute = [
+alignment = edist.alignment.Alignment()
+to_compute = deque([
     (1, 1)
-]
+])
 trace_so_far = []
 
-while to_compute:
+while len(to_compute) > 0:
     i, j = to_compute.pop()
     ((f_match, f_arg), (t_match, t_arg)) = trace[i][j]
 
+    print(trace[i][j])
     if t_match == Cmd.MATCH:
-        trace_so_far.append((i-1, j-1))
+        alignment.append_tuple(i-1, j-1)
 
         if f_match == Cmd.MATCH:
-            for tpl in f_arg:
-                next = (a_adj[i-1][tpl._left]+1, b_adj[j-1][tpl._right]+1)
+            for tpl in reversed(f_arg):
+                if tpl._right < 0:
+                    alignment.append_tuple(a_adj[i-1][tpl._left], -1)
+                elif tpl._left < 0:
+                    alignment.append_tuple(0, b_adj[j-1][tpl._right]+1)
+                else:
+                    next = (a_adj[i-1][tpl._left]+1, b_adj[j-1][tpl._right]+1)
+                    print(f"Scheduling {next}")
+                    to_compute.append(next)
+        elif f_match == Cmd.ADD:
+            for c in reversed(a_adj[i-1]):
+                next = (i, b_adj[j-1][f_arg]+1)
+                print(f"Scheduling {next}")
                 to_compute.append(next)
-        else:
-            adj = b_adj if f_match == Cmd.ADD else a_adj
-            for c in adj[adj[j-1][f_arg]]:
-                next = (i, adj[j-1][f_arg]+1) if f_match == Cmd.ADD else (adj[i-1][f_arg]+1, j)
+        elif f_match == Cmd.REMOVE:
+            for c in reversed(b_adj[j]):
+                next = (a_adj[i-1][f_arg]+1, j)
+                print(f"Scheduling {next}")
                 to_compute.append(next)
+    elif t_match == Cmd.REMOVE:
+        alignment.append_tuple(i-1, -1)
+        next = (a_adj[i-1][t_arg]+1, j)
+        print(f"Scheduling {next}")
+        to_compute.append(next)
+    elif t_match == Cmd.ADD:
+        alignment.append_tuple(-1, j-1)
+        next = (i, b_adj[j-1][t_arg]+1)
+        print(f"Scheduling {next}")
+        to_compute.append(next)
 
-print(trace_so_far)
+print(alignment)
+script = edist.tree_edits.alignment_to_script(alignment, a, a_adj, b, b_adj)
+print(script)
+print(script.apply(a, a_adj))
+
+print('\n'.join([''.join(['{:80}'.format(str(item)) for item in row]) 
+      for row in trace]))
