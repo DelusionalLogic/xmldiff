75129814e2bda371a3a86c1fda3523315a47fbb6
Jesper Jensen <jesper@jnsn.dev>
2024-09-09 19:10:30 +0200

    fix: Update test cases for constrained edit distance

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ class TestConstrainedEditDistance(unittest.TestCase):
         a_adj = [[1, 2], [], []]
         b_adj = [[1], [2], []]
         distance, _ = constrained_edit_distance(a_adj, b_adj, cost, [list(range(len(a_adj)))]*2)
-        self.assertEqual(distance, 1)
+        self.assertEqual(distance, 2)
 
     def test_add_node(self):
         a_adj = [[1], []]
@@ -46,10 +46,10 @@ class TestConstrainedEditDistance(unittest.TestCase):
         self.assertEqual(distance, 0)
 
     def test_multi_level_trees(self):
-        a_adj = [[1, 2], [3, 4], [], [], []]
-        b_adj = [[1, 2], [3], [4], [], []]
+        a_adj = [[1, 4], [2, 3], [], [], []]
+        b_adj = [[1, 3], [2], [], [4], []]
         distance, _ = constrained_edit_distance(a_adj, b_adj, cost, [list(range(len(a_adj)))]*2)
-        self.assertEqual(distance, 1)
+        self.assertEqual(distance, 2)
 
     def test_different_node_values(self):
         a_adj = [[1, 2], [], []]
@@ -57,12 +57,6 @@ class TestConstrainedEditDistance(unittest.TestCase):
         a_values = [0, 1, 2]
         b_values = [0, 2, 1]
         distance, _ = constrained_edit_distance(a_adj, b_adj, cost, [a_values, b_values])
-        self.assertEqual(distance, 2)
-
-    def test_completely_different_trees(self):
-        a_adj = [[1, 2], [], []]
-        b_adj = [[1], [2], []]
-        distance, _ = constrained_edit_distance(a_adj, b_adj, cost, [list(range(len(a_adj))), list(range(len(b_adj)))])
         self.assertEqual(distance, 2)
 
 if __name__ == '__main__':
